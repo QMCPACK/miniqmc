e8c506bddff0b1104004d2ebafd5142b98625bca
Graham Lopez <lopezmg@ornl.gov>
2017-08-03 13:25:48 -0400

    formatted OhmmsSoA directory

--- src/OhmmsSoA/Container.h
+++ src/OhmmsSoA/Container.h
@@ -1,17 +1,18 @@
 //////////////////////////////////////////////////////////////////////////////////////
-// This file is distributed under the University of Illinois/NCSA Open Source License.
+// This file is distributed under the University of Illinois/NCSA Open Source
+// License.
 // See LICENSE file in top directory for details.
 //
 // Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
 //
-// File developed by: 
+// File developed by:
 //
 // File created by: Jeongnim Kim, jeongnim.kim@intel.com, Intel Corp.
 //////////////////////////////////////////////////////////////////////////////////////
 // -*- C++ -*-
 /** @file Container.h
  *
- * Master header to support SoA containers 
+ * Master header to support SoA containers
  */
 #ifndef QMCPLUSPLUS_SOA_CONTAINER_H
 #define QMCPLUSPLUS_SOA_CONTAINER_H
