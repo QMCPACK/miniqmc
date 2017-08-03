be0163dc47cf7ba588fd54f5f5d16b92763ed281
Graham Lopez <lopezmg@ornl.gov>
2017-08-03 11:39:05 -0400

    Message directory formatted

--- src/Message/OpenMP.h
+++ src/Message/OpenMP.h
@@ -1,19 +1,18 @@
 //////////////////////////////////////////////////////////////////////////////////////
-// This file is distributed under the University of Illinois/NCSA Open Source License.
+// This file is distributed under the University of Illinois/NCSA Open Source
+// License.
 // See LICENSE file in top directory for details.
 //
 // Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
 //
-// File developed by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
-//                    Jeremy McMinnis, jmcminis@gmail.com, University of Illinois at Urbana-Champaign
+// File developed by: Jeongnim Kim, jeongnim.kim@gmail.com, University of
+// Illinois at Urbana-Champaign
+//                    Jeremy McMinnis, jmcminis@gmail.com, University of
+//                    Illinois at Urbana-Champaign
 //
-// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois at Urbana-Champaign
+// File created by: Jeongnim Kim, jeongnim.kim@gmail.com, University of Illinois
+// at Urbana-Champaign
 //////////////////////////////////////////////////////////////////////////////////////
-    
-    
-
-
-
 
 #ifndef OHMMS_OPENMP_H
 #define OHMMS_OPENMP_H
@@ -26,17 +25,8 @@
 #include <omp.h>
 #else
 typedef int omp_int_t;
-inline omp_int_t omp_get_thread_num()
-{
-  return 0;
-}
-inline omp_int_t omp_get_max_threads()
-{
-  return 1;
-}
-inline omp_int_t omp_get_num_threads()
-{
-  return 1;
-}
+inline omp_int_t omp_get_thread_num() { return 0; }
+inline omp_int_t omp_get_max_threads() { return 1; }
+inline omp_int_t omp_get_num_threads() { return 1; }
 #endif
 #endif // OHMMS_COMMUNICATE_H
