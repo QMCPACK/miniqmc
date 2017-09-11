be19a65b28b7eda33bf28362eb5cd456b4260119
Ye Luo <yeluo@anl.gov>
2017-09-11 18:58:26 -0500

    Forget to add Mover.hpp

---
+++ src/miniapps/Mover.hpp
@@ -0,0 +1,27 @@
+#ifndef QMCPLUSPLUS_WALKER_HPP
+#define QMCPLUSPLUS_WALKER_HPP
+
+#include <Configuration.h>
+#include <Particle/ParticleSet.h>
+#include <Utilities/RandomGenerator.h>
+#include <miniapps/pseudo.hpp>
+#include <spline2/MultiBsplineRef.hpp>
+#include <QMCWaveFunctions/einspline_spo.hpp>
+
+namespace qmcplusplus
+{
+  struct Mover
+  {
+    using RealType = QMCTraits::RealType;
+    using spo_type = einspline_spo<RealType, MultiBspline<RealType> >;
+    using spo_ref_type = einspline_spo<RealType, MultiBsplineRef<RealType> >;
+
+    RandomGenerator<RealType> *rng;
+    ParticleSet               *els;
+    spo_type                  *spo;
+    spo_ref_type              *spo_ref;
+    NonLocalPP<RealType>      *nlpp;
+  };
+}
+
+#endif
