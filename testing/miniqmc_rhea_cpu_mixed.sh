f2e06dcc8124d91c9858b8445274dc3bec8bbc0f
Graham Lopez <lopezmg@ornl.gov>
2017-12-14 12:39:21 -0500

    fix CI logic to handle better output messages

--- testing/miniqmc_rhea_cpu_mixed.sh
+++ testing/miniqmc_rhea_cpu_mixed.sh
@@ -15,7 +15,7 @@ cd $BUILD_DIR
 
 source /sw/rhea/environment-modules/3.2.10/rhel6.7_gnu4.4.7/init/bash
 
-module unload PE-intel 
+module unload PE-intel
 module load PE-gnu/5.3.0-1.10.2
 module load git
 
@@ -23,7 +23,7 @@ env
 
 module list
 
-cd build 
+cd build
 
 cmake -D QMC_MIXED_PRECISION=1 -DCMAKE_C_COMPILER="mpicc" -DCMAKE_CXX_COMPILER="mpicxx" -DCMAKE_CXX_FLAGS="-std=c++11" -DBLAS_blas_LIBRARY="/usr/lib64/libblas.so.3" -DLAPACK_lapack_LIBRARY="/usr/lib64/atlas/liblapack.so.3" ..
 
@@ -44,11 +44,11 @@ echo
 ./bin/check_wfc -f J2
 
 echo
-echo checking JeeI
+echo checking J3
 echo ----------------------------------------------------
 echo
 
-./bin/check_wfc -f JeeI
+./bin/check_wfc -f J3
 
 echo
 echo checking Spline SPO
@@ -64,11 +64,11 @@ cp $BUILD_TAG.pbs $BUILD_DIR
 cd $BUILD_DIR
 
 source scl_source enable rh-python35
-which python 
+which python
 
 $BUILD_DIR/../../../scripts/blocking_qsub.py $BUILD_DIR $BUILD_TAG.pbs
 
 cp $BUILD_DIR/$BUILD_TAG.o* ../
 
 # get status from checks
-[ $(grep 'All checking pass!' ../$BUILD_TAG.o* | wc -l) -eq 4 ]
+[ $(grep -e 'All checking pass for J[123]' -e 'All checking pass for spo' ../$BUILD_TAG.o* | wc -l) -eq 4 ]
