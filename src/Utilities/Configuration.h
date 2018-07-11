10ca92d313a42486cc8db44b7db6d176ba6cf7ce
rcclay <j.k.rofling@gmail.com>
2018-07-11 11:11:58 -0600

    Kokkos will handle OpenMP.

--- src/Utilities/Configuration.h
+++ src/Utilities/Configuration.h
@@ -37,16 +37,19 @@
     exit(1);                                                      \
   }
 
-#if defined(ENABLE_OPENMP)
-#include <omp.h>
-#else
-typedef int omp_int_t;
-inline omp_int_t omp_get_thread_num() { return 0; }
-inline omp_int_t omp_get_max_threads() { return 1; }
-inline omp_int_t omp_get_num_threads() { return 1; }
+#if !defined(QMC_USE_KOKKOS)
+  #if defined(ENABLE_OPENMP)
+    #include <omp.h>
+  #else
+    typedef int omp_int_t;
+    inline omp_int_t omp_get_thread_num() { return 0; }
+    inline omp_int_t omp_get_max_threads() { return 1; }
+    inline omp_int_t omp_get_num_threads() { return 1; }
+  #endif
 #endif
 
 #if defined(QMC_USE_KOKKOS)
+
 #include <Kokkos_Core.hpp>
 #endif
 
