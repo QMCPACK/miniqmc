3c3c9475a3edbf786edc8c41ddd627b7f6dc0966
Peter Doak <doakpw@ornl.gov>
2018-08-02 15:42:26 -0400

    protecting Blasf.h from clang-format separate Numerics .clang-format

--- src/Numerics/Spline2/MultiBsplineData.cpp
+++ src/Numerics/Spline2/MultiBsplineData.cpp
@@ -20,56 +20,37 @@
 namespace qmcplusplus
 {
 /** initialization of static data for MultiBsplineData<float> */
-template<>
-QMC_ALIGNAS const float MultiBsplineData<float>::A44[16] = {-1.0f / 6.0f,
-                                                            3.0f / 6.0f,
-                                                            -3.0f / 6.0f,
-                                                            1.0f / 6.0f,
-                                                            3.0f / 6.0f,
-                                                            -6.0f / 6.0f,
-                                                            0.0f / 6.0f,
-                                                            4.0f / 6.0f,
-                                                            -3.0f / 6.0f,
-                                                            3.0f / 6.0f,
-                                                            3.0f / 6.0f,
-                                                            1.0f / 6.0f,
-                                                            1.0f / 6.0f,
-                                                            0.0f / 6.0f,
-                                                            0.0f / 6.0f,
-                                                            0.0f / 6.0f};
+template <>
+QMC_ALIGNAS const float MultiBsplineData<float>::A44[16] = {
+    -1.0f / 6.0f, 3.0f / 6.0f,  -3.0f / 6.0f, 1.0f / 6.0f,
+    3.0f / 6.0f,  -6.0f / 6.0f, 0.0f / 6.0f,  4.0f / 6.0f,
+    -3.0f / 6.0f, 3.0f / 6.0f,  3.0f / 6.0f,  1.0f / 6.0f,
+    1.0f / 6.0f,  0.0f / 6.0f,  0.0f / 6.0f,  0.0f / 6.0f};
 
-template<>
-QMC_ALIGNAS const float MultiBsplineData<float>::dA44[16] =
-    {0.0f, -0.5f, 1.0f, -0.5f, 0.0f, 1.5f, -2.0f, 0.0f, 0.0f, -1.5f, 1.0f, 0.5f, 0.0f, 0.5f, 0.0f, 0.0f};
+template <>
+QMC_ALIGNAS const float MultiBsplineData<float>::dA44[16] = {
+    0.0f, -0.5f, 1.0f, -0.5f, 0.0f, 1.5f, -2.0f, 0.0f,
+    0.0f, -1.5f, 1.0f, 0.5f,  0.0f, 0.5f, 0.0f,  0.0f};
 
-template<>
-QMC_ALIGNAS const float MultiBsplineData<float>::d2A44[16] =
-    {0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 3.0f, -2.0f, 0.0f, 0.0f, -3.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
+template <>
+QMC_ALIGNAS const float MultiBsplineData<float>::d2A44[16] = {
+    0.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 3.0f, -2.0f,
+    0.0f, 0.0f, -3.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
 
 /** initialization of static data for MultiBsplineData<double> */
-template<>
-QMC_ALIGNAS const double MultiBsplineData<double>::A44[16] = {-1.0 / 6.0,
-                                                              3.0 / 6.0,
-                                                              -3.0 / 6.0,
-                                                              1.0 / 6.0,
-                                                              3.0 / 6.0,
-                                                              -6.0 / 6.0,
-                                                              0.0 / 6.0,
-                                                              4.0 / 6.0,
-                                                              -3.0 / 6.0,
-                                                              3.0 / 6.0,
-                                                              3.0 / 6.0,
-                                                              1.0 / 6.0,
-                                                              1.0 / 6.0,
-                                                              0.0 / 6.0,
-                                                              0.0 / 6.0,
-                                                              0.0 / 6.0};
+template <>
+QMC_ALIGNAS const double MultiBsplineData<double>::A44[16] = {
+    -1.0 / 6.0, 3.0 / 6.0, -3.0 / 6.0, 1.0 / 6.0, 3.0 / 6.0, -6.0 / 6.0,
+    0.0 / 6.0,  4.0 / 6.0, -3.0 / 6.0, 3.0 / 6.0, 3.0 / 6.0, 1.0 / 6.0,
+    1.0 / 6.0,  0.0 / 6.0, 0.0 / 6.0,  0.0 / 6.0};
 
-template<>
-QMC_ALIGNAS const double MultiBsplineData<double>::dA44[16] =
-    {0.0, -0.5, 1.0, -0.5, 0.0, 1.5, -2.0, 0.0, 0.0, -1.5, 1.0, 0.5, 0.0, 0.5, 0.0, 0.0};
+template <>
+QMC_ALIGNAS const double MultiBsplineData<double>::dA44[16] = {
+    0.0, -0.5, 1.0, -0.5, 0.0, 1.5, -2.0, 0.0,
+    0.0, -1.5, 1.0, 0.5,  0.0, 0.5, 0.0,  0.0};
 
-template<>
-QMC_ALIGNAS const double MultiBsplineData<double>::d2A44[16] =
-    {0.0, 0.0, -1.0, 1.0, 0.0, 0.0, 3.0, -2.0, 0.0, 0.0, -3.0, 1.0, 0.0, 0.0, 1.0, 0.0};
-} // namespace qmcplusplus
+template <>
+QMC_ALIGNAS const double MultiBsplineData<double>::d2A44[16] = {
+    0.0, 0.0, -1.0, 1.0, 0.0, 0.0, 3.0, -2.0,
+    0.0, 0.0, -3.0, 1.0, 0.0, 0.0, 1.0, 0.0};
+}
