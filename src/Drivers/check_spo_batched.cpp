ea6518d125151c0fa62c5710460cce365f8e638e
Ye Luo <yeluo@anl.gov>
2018-09-04 17:50:58 -0500

    Rename check_spo as check_spo_batched.

--- src/Drivers/check_spo_batched.cpp
+++ src/Drivers/check_spo_batched.cpp
@@ -20,6 +20,8 @@
  */
 #include <Utilities/Configuration.h>
 #include <Utilities/Communicate.h>
+#include <Utilities/NewTimer.h>
+#include <Drivers/Mover.hpp>
 #include <Particle/ParticleSet.h>
 #include <Particle/ParticleSet_builder.hpp>
 #include <Utilities/RandomGenerator.h>
@@ -32,14 +34,34 @@
 using namespace std;
 using namespace qmcplusplus;
 
+enum CheckSPOTimers
+{
+  Timer_Total,
+  Timer_Init,
+  Timer_SPO_v,
+  Timer_SPO_vgh,
+  Timer_SPO_ref_v,
+  Timer_SPO_ref_vgh
+};
+
+TimerNameList_t<CheckSPOTimers> CheckSPOTimerNames = {
+    {Timer_Total, "Total"},
+    {Timer_Init, "Initialization"},
+    {Timer_SPO_v, "SPO_v"},
+    {Timer_SPO_vgh, "SPO_vgh"},
+    {Timer_SPO_ref_v, "SPO_ref_v"},
+    {Timer_SPO_ref_vgh, "SPO_ref_vgh"},
+};
+
 void print_help()
 {
   // clang-format off
   app_summary() << "usage:" << '\n';
-  app_summary() << "  check_spo [-hvV] [-g \"n0 n1 n2\"] [-m meshfactor]"        << '\n';
+  app_summary() << "  check_spo [-fhvV] [-g \"n0 n1 n2\"] [-m meshfactor]"       << '\n';
   app_summary() << "            [-n steps] [-r rmax] [-s seed]"                  << '\n';
   app_summary() << "options:"                                                    << '\n';
   app_summary() << "  -g  set the 3D tiling.             default: 1 1 1"         << '\n';
+  app_summary() << "  -f  results are not transfered back"                       << '\n';
   app_summary() << "  -h  print help and exit"                                   << '\n';
   app_summary() << "  -m  meshfactor                     default: 1.0"           << '\n';
   app_summary() << "  -n  number of MC steps             default: 5"             << '\n';
@@ -64,19 +86,18 @@ int main(int argc, char** argv)
 
   // use the global generator
 
-  int na     = 1;
-  int nb     = 1;
-  int nc     = 1;
-  int nsteps = 5;
-  int iseed  = 11;
+  int na      = 1;
+  int nb      = 1;
+  int nc      = 1;
+  int nsteps  = 5;
+  int nmovers = omp_get_max_threads();
+  int iseed   = 11;
+  // this is the cutoff from the non-local PP
   RealType Rmax(1.7);
   int nx = 37, ny = 37, nz = 37;
-  // thread blocking
-  // int team_size=1; //default is 1
   int tileSize  = -1;
-  int team_size = 1;
-
-  bool verbose = false;
+  bool transfer = true;
+  bool verbose  = false;
 
   if (!comm.root())
   {
@@ -86,18 +107,19 @@ int main(int argc, char** argv)
   int opt;
   while (optind < argc)
   {
-    if ((opt = getopt(argc, argv, "hvVa:c:f:g:m:n:r:s:")) != -1)
+    if ((opt = getopt(argc, argv, "fhvVa:g:m:n:r:s:w:")) != -1)
     {
       switch (opt)
       {
       case 'a':
         tileSize = atoi(optarg);
         break;
-      case 'c': // number of members per team
-        team_size = atoi(optarg);
-        break;
       case 'g': // tiling1 tiling2 tiling3
         sscanf(optarg, "%d %d %d", &na, &nb, &nc);
+        break;
+      case 'f': // forward only, no transfer back to host
+        printf("Results are not transferred back. Checking report should be ignored.\n");
+        transfer = false;
         break;
       case 'h':
         print_help();
@@ -110,7 +132,7 @@ int main(int argc, char** argv)
         nz *= meshfactor;
       }
       break;
-      case 'n':
+      case 'n': // number of MC steps
         nsteps = atoi(optarg);
         break;
       case 'r': // rmax
@@ -125,6 +147,9 @@ int main(int argc, char** argv)
       case 'V':
         print_version(true);
         return 1;
+        break;
+      case 'w': // number of nmovers
+        nmovers = atoi(optarg);
         break;
       default:
         print_help();
@@ -147,15 +172,19 @@ int main(int argc, char** argv)
 
   print_version(verbose);
 
-  Tensor<int, 3> tmat(na, 0, 0, 0, nb, 0, 0, 0, nc);
+  TimerManager.set_timer_threshold(timer_level_fine);
+  TimerList_t Timers;
+  setup_timers(Timers, CheckSPOTimerNames, timer_level_coarse);
 
-  OHMMS_PRECISION ratio = 0.0;
+  Timers[Timer_Total]->start();
+  Timers[Timer_Init]->start();
 
   using spo_type = einspline_spo<OHMMS_PRECISION>;
   spo_type spo_main;
   using spo_ref_type = miniqmcreference::einspline_spo_ref<OHMMS_PRECISION>;
   spo_ref_type spo_ref_main;
   int nTiles = 1;
+  Tensor<int, 3> tmat(na, 0, 0, 0, nb, 0, 0, 0, nc);
 
   ParticleSet ions;
   // initialize ions and splines which are shared by all threads later
@@ -189,70 +218,104 @@ int main(int argc, char** argv)
     spo_ref_main.Lattice.set(lattice_b);
   }
 
-  double nspheremoves = 0;
-  double dNumVGHCalls = 0;
+  const int nions = ions.getTotalNum();
+  const int nels  = count_electrons(ions, 1);
+  const int nels3 = 3 * nels;
 
+  // construct a list of movers
+  std::vector<Mover*> mover_list(nmovers, nullptr);
+  std::cout << "Constructing " << nmovers << " movers!" << std::endl;
+  std::vector<spo_ref_type*> spo_ref_views(nmovers, nullptr);
+  // per mover data
+  std::vector<ParticlePos_t> delta_list(nmovers);
+  std::vector<ParticlePos_t> rOnSphere_list(nmovers);
+  std::vector<std::vector<RealType>> ur_list(nmovers);
+  std::vector<int> my_accepted_list(nmovers), my_vals_list(nmovers);
+  std::vector<PosType> pos_list(nmovers);
+  std::vector<SPOSet*> spo_shadows(nmovers);
+
+#pragma omp parallel for
+  for (size_t iw = 0; iw < mover_list.size(); iw++)
+  {
+    // create and initialize movers
+    Mover* thiswalker = new Mover(MakeSeed(iw, mover_list.size()), ions);
+    mover_list[iw]    = thiswalker;
+
+    // create a spo view in each Mover
+    thiswalker->spo   = dynamic_cast<SPOSet*>(new spo_type(spo_main, 1, 0));
+    spo_shadows[iw]   = thiswalker->spo;
+    spo_ref_views[iw] = new spo_ref_type(spo_ref_main, 1, 0);
+
+    // initial computing
+    thiswalker->els.update();
+
+    // temporal data during walking
+    delta_list[iw].resize(nels);
+    rOnSphere_list[iw].resize(thiswalker->nlpp.size());
+    ur_list[iw].resize(nels);
+  }
+
+  Timers[Timer_Init]->stop();
+
+  RealType sqrttau = 2.0;
+  RealType accept  = 0.5;
+
+  const double zval = 1.0 * static_cast<double>(nels) / static_cast<double>(nions);
+
+  double ratio         = 0.0;
+  double nspheremoves  = 0.0;
+  double dNumVGHCalls  = 0.0;
   double evalV_v_err   = 0.0;
   double evalVGH_v_err = 0.0;
   double evalVGH_g_err = 0.0;
   double evalVGH_h_err = 0.0;
 
-  // clang-format off
-  #pragma omp parallel reduction(+:ratio,nspheremoves,dNumVGHCalls) \
-   reduction(+:evalV_v_err,evalVGH_v_err,evalVGH_g_err,evalVGH_h_err)
-  // clang-format on
+  for (int mc = 0; mc < nsteps; ++mc)
   {
-    const int np        = omp_get_num_threads();
-    const int ip        = omp_get_thread_num();
-    const int team_id   = ip / team_size;
-    const int member_id = ip % team_size;
-
-    // create generator within the thread
-    RandomGenerator<RealType> random_th(MakeSeed(team_id, np));
-
-    ParticleSet els;
-    build_els(els, ions, random_th);
-    els.update();
-
-    const int nions = ions.getTotalNum();
-    const int nels  = els.getTotalNum();
-    const int nels3 = 3 * nels;
-
-    // create pseudopp
-    NonLocalPP<OHMMS_PRECISION> ecp(random_th);
-    // create spo per thread
-    spo_type spo(spo_main, team_size, member_id);
-    spo_ref_type spo_ref(spo_ref_main, team_size, member_id);
-
-    // use teams
-    // if(team_size>1 && team_size>=nTiles ) spo.set_range(team_size,ip%team_size);
-
-    // this is the cutoff from the non-local PP
-    const int nknots(ecp.size());
-
-    ParticlePos_t delta(nels);
-    ParticlePos_t rOnSphere(nknots);
-
-    RealType sqrttau = 2.0;
-    RealType accept  = 0.5;
-
-    vector<RealType> ur(nels);
-    random_th.generate_uniform(ur.data(), nels);
-    const double zval = 1.0 * static_cast<double>(nels) / static_cast<double>(nions);
-
-    int my_accepted = 0, my_vals = 0;
-
-    for (int mc = 0; mc < nsteps; ++mc)
+    std::cout << "mc = " << mc << std::endl;
+    for (size_t iw = 0; iw < mover_list.size(); iw++)
     {
+      auto& mover     = *mover_list[iw];
+      auto& random_th = mover.rng;
+      auto& delta     = delta_list[iw];
+      auto& ur        = ur_list[iw];
+
       random_th.generate_normal(&delta[0][0], nels3);
       random_th.generate_uniform(ur.data(), nels);
+    }
 
-      // VMC
-      for (int iel = 0; iel < nels; ++iel)
+    spo_type* anon_spo = dynamic_cast<spo_type*>(mover_list[0]->spo);
+    // VMC
+    for (int iel = 0; iel < nels; ++iel)
+    {
+      for (size_t iw = 0; iw < mover_list.size(); iw++)
       {
-        PosType pos = els.R[iel] + sqrttau * delta[iel];
-        spo.evaluate_vgh(pos);
-        spo_ref.evaluate_vgh(pos);
+        auto& els   = mover_list[iw]->els;
+        auto& delta = delta_list[iw];
+        auto& pos   = pos_list[iw];
+        pos         = els.R[iel] + sqrttau * delta[iel];
+      }
+
+      Timers[Timer_SPO_vgh]->start();
+      anon_spo->multi_evaluate_vgh(spo_shadows, pos_list);
+      Timers[Timer_SPO_vgh]->stop();
+      if (transfer)
+        anon_spo->multi_transfer_from_device(spo_shadows);
+
+#pragma omp parallel for reduction(+ : evalVGH_v_err, evalVGH_g_err, evalVGH_h_err)
+      for (size_t iw = 0; iw < mover_list.size(); iw++)
+      {
+        auto& mover       = *mover_list[iw];
+        auto& spo         = *dynamic_cast<spo_type*>(mover.spo);
+        auto& spo_ref     = *spo_ref_views[iw];
+        auto& pos         = pos_list[iw];
+        auto& els         = mover.els;
+        auto& ur          = ur_list[iw];
+        auto& my_accepted = my_accepted_list[iw];
+        Timers[Timer_SPO_ref_vgh]->start();
+        if (transfer)
+          spo_ref.evaluate_vgh(pos);
+        Timers[Timer_SPO_ref_vgh]->stop();
         // accumulate error
         for (int ib = 0; ib < spo.nBlocks; ib++)
           for (int n = 0; n < spo.nSplinesPerBlock; n++)
@@ -277,7 +340,14 @@ int main(int argc, char** argv)
           my_accepted++;
         }
       }
+    }
 
+#if 0
+#pragma omp parallel for reduction(+ : evalV_v_err)
+        for(size_t iw = 0; iw < mover_list.size(); iw++)
+      // TODO: move in mover loop
+      auto &my_vals = my_vals_list[iw];
+      my_vals = 0
       random_th.generate_uniform(ur.data(), nels);
       ecp.randomize(rOnSphere); // pick random sphere
       for (int iat = 0, kat = 0; iat < nions; ++iat)
@@ -285,11 +355,11 @@ int main(int argc, char** argv)
         const int nnF = static_cast<int>(ur[kat++] * zval);
         RealType r    = Rmax * ur[kat++];
         auto centerP  = ions.R[iat];
-        my_vals += (nnF * nknots);
+        my_vals += (nnF * ecp.size());
 
         for (int nn = 0; nn < nnF; ++nn)
         {
-          for (int k = 0; k < nknots; k++)
+          for (int k = 0; k < ecp.size(); k++)
           {
             PosType pos = centerP + r * rOnSphere[k];
             spo.evaluate_v(pos);
@@ -300,24 +370,45 @@ int main(int argc, char** argv)
                 evalV_v_err += std::fabs(spo.psi[ib][n] - spo_ref.psi[ib][n]);
           }
         } // els
-      }   // ions
+      } // ions
+#endif
 
-    } // steps.
+  } // steps.
 
-    ratio        += RealType(my_accepted) / RealType(nels * nsteps);
-    nspheremoves += RealType(my_vals) / RealType(nsteps);
-    dNumVGHCalls += nels;
+  Timers[Timer_Total]->stop();
 
-  } // end of omp parallel
+// free all movers
+#pragma omp parallel for
+  for (int iw = 0; iw < nmovers; iw++)
+  {
+    delete mover_list[iw];
+    delete spo_ref_views[iw];
+  }
+  mover_list.clear();
+  spo_ref_views.clear();
 
   outputManager.resume();
 
-  evalV_v_err   /= nspheremoves;
+  if (comm.root())
+  {
+    cout << "================================== " << endl;
+    TimerManager.print();
+    cout << "================================== " << endl;
+  }
+
+  for (size_t iw = 0; iw < nmovers; iw++)
+  {
+    ratio += RealType(my_accepted_list[iw]) / RealType(nels * nsteps);
+    nspheremoves += RealType(my_vals_list[iw]) / RealType(nsteps);
+    dNumVGHCalls += nels;
+  }
+
+  //evalV_v_err /= nspheremoves;
   evalVGH_v_err /= dNumVGHCalls;
   evalVGH_g_err /= dNumVGHCalls;
   evalVGH_h_err /= dNumVGHCalls;
 
-  int np                     = omp_get_max_threads();
+  const int np               = nmovers;
   constexpr RealType small_v = std::numeric_limits<RealType>::epsilon() * 1e4;
   constexpr RealType small_g = std::numeric_limits<RealType>::epsilon() * 3e6;
   constexpr RealType small_h = std::numeric_limits<RealType>::epsilon() * 6e8;
