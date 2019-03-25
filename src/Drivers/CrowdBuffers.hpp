e353a072fce59438e00b339f0b247132fe9c83e1
Peter Doak <doakpw@ornl.gov>
2019-03-25 13:58:12 -0400

    hack to retain generic code

---
+++ src/Drivers/CrowdBuffers.hpp
@@ -0,0 +1,28 @@
+
+#ifndef QMCPLUSPLUS_CROWD_BUFFERS_HPP
+#define QMCPLUSPLUS_CROWD_BUFFERS_HPP
+
+namespace qmcplusplus
+{
+template<Devices DT>
+class CrowdBuffers
+{
+
+};
+
+#ifdef QMC_USE_CUDA
+#include "CUDA/GPUArray.h"
+template<>
+class CrowdBuffers<Devices::CUDA>
+{
+public:
+  using T = double;
+  
+  GPUArray<T, 1, 1> dev_psi;
+  GPUArray<T, 3, 1> dev_grad;
+  GPUArray<T, 6, 1> dev_hess;
+};
+#endif
+}
+
+#endif
