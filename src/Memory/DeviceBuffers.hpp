1fd171b8628045ddbb1422b46c84aa904679c4b0
Peter Doak <doakpw@ornl.gov>
2019-04-05 15:13:55 -0400

    forgot new files per usual

---
+++ src/Memory/DeviceBuffers.hpp
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source
+// License.  See LICENSE file in top directory for details.
+//
+// Copyright (c) 2019 QMCPACK developers.
+//
+// File developed by:
+// Peter Doak, doakpw@ornl.gov, Oak Ridge National Lab
+//
+// File created by:
+// Peter Doak, doakpw@ornl.gov, Oak Ridge National Lab
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef QMCPLUSPLUS_DEVICE_BUFFERS_HPP
+#define QMCPLUSPLUS_DEVICE_BUFFERS_HPP
+
+#include "Devices.h"
+#include "CUDA/PinnedHostBuffer.hpp"
+/** Devices require all sorts of buffers, this is a pretty generic container to hold them.
+ *  Specialized versions of the class template can contain whatever specialized methods useing this need
+ */
+
+namespace qmcplusplus
+{
+template <Devices DT>
+struct DeviceBuffers
+{    
+};
+
+#ifdef QMC_USE_CUDA
+/** This is used to share one pinned host buffer between all the dterminants in a crowd
+ *  having many in play at once seems to cause performance issues
+ */
+template<>
+struct DeviceBuffers<Devices::CUDA>
+{
+  PinnedHostBuffer determinant_host_buffer;
+};
+#endif
+}
+
+#endif
---
+++ src/QMCWaveFunctions/WaveFunctionBuilderCUDA.hpp
@@ -0,0 +1,23 @@
+#ifndef QMCPLUSPLUS_WAVEFUNCTION_BUILDER_CUDA_H
+#define QMCPLUSPLUS_WAVEFUNCTION_BUILDER_CUDA_H
+#include "Devices.h"
+#include "QMCWaveFunctions/WaveFunction.h"
+#include "QMCWaveFunctions/Determinant.h"
+#include "QMCWaveFunctions/DeterminantDeviceImp.h"
+
+#include "QMCWaveFunctions/WaveFunctionBuilder.h"
+namespace qmcplusplus
+{
+    template <>
+    inline void WaveFunctionBuilder<Devices::CUDA>::devDetBuild(WaveFunction& WF,const RandomGenerator<QMCTraits::RealType>& RNG,                                     ParticleSet& els,DeviceBuffers<Devices::CUDA>& dev_bufs)
+    {
+	using DetType   = DiracDeterminant<DeterminantDeviceImp<Devices::CUDA>>;
+
+	    // determinant component
+	WF.Det_up = new DetType(WF.nelup, RNG, dev_bufs, 0);
+	WF.Det_dn = new DetType(els.getTotalNum() - WF.nelup, RNG, dev_bufs, WF.nelup); 
+    }
+
+}
+
+#endif
