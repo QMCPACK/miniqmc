5307b4f77d7800fddec7f97a1c635204b79a5589
Ye Luo <yeluo@anl.gov>
2019-04-19 13:19:56 -0500

    Bugfix R[iel] to activePos.

--- src/QMCWaveFunctions/WaveFunction.cpp
+++ src/QMCWaveFunctions/WaveFunction.cpp
@@ -206,7 +206,7 @@ WaveFunction::posT WaveFunction::evalGrad(ParticleSet& P, int iat)
 
 WaveFunction::valT WaveFunction::ratioGrad(ParticleSet& P, int iat, posT& grad)
 {
-  spo->evaluate_vgh(P.R[iat]);
+  spo->evaluate_vgh(P.activePos);
 
   timers[Timer_Det]->start();
   grad       = valT(0);
@@ -224,7 +224,7 @@ WaveFunction::valT WaveFunction::ratioGrad(ParticleSet& P, int iat, posT& grad)
 
 WaveFunction::valT WaveFunction::ratio(ParticleSet& P, int iat)
 {
-  spo->evaluate_v(P.R[iat]);
+  spo->evaluate_v(P.activePos);
 
   timers[Timer_Det]->start();
   valT ratio = (iat < nelup ? Det_up->ratio(P, iat) : Det_dn->ratio(P, iat));
@@ -373,7 +373,7 @@ void WaveFunction::flex_ratioGrad(const std::vector<WaveFunction*>& WF_list,
   {
     std::vector<posT> pos_list(P_list.size());
     for (int iw = 0; iw < P_list.size(); iw++)
-      pos_list[iw] = P_list[iw]->R[iat];
+      pos_list[iw] = P_list[iw]->activePos;
     auto spo_list(extract_spo_list(WF_list));
     spo->multi_evaluate_vgh(spo_list, pos_list);
 
