c513d9337a1ff53251ba5b2331d8e5ff806f8f14
Ye Luo <yeluo@anl.gov>
2019-06-12 19:12:15 -0500

    Align miniqmc_sync_move options.

--- src/Drivers/miniqmc_sync_move.cpp
+++ src/Drivers/miniqmc_sync_move.cpp
@@ -153,18 +153,18 @@ void print_help()
   app_summary() << "  miniqmc   [-bhjPvV] [-g \"n0 n1 n2\"] [-m meshfactor]"     << '\n';
   app_summary() << "            [-n steps] [-N substeps] [-x rmax]"              << '\n';
   app_summary() << "            [-r AcceptanceRatio] [-s seed] [-w walkers]"     << '\n';
-  app_summary() << "            [-a tile_size] [-t timer_level] [-B nw_b]"       << '\n';
+  app_summary() << "            [-a tile_size] [-t timer_level] [-p nw_b]"       << '\n';
   app_summary() << "            [-k delay_rank]"                                 << '\n';
   app_summary() << "options:"                                                    << '\n';
   app_summary() << "  -a  size of each spline tile       default: num of orbs"   << '\n';
   app_summary() << "  -b  use reference implementations  default: off"           << '\n';
-  app_summary() << "  -B  number of walkers per batch    default: 1"             << '\n';
   app_summary() << "  -g  set the 3D tiling.             default: 1 1 1"         << '\n';
   app_summary() << "  -h  print help and exit"                                   << '\n';
   app_summary() << "  -j  enable three body Jastrow      default: off"           << '\n';
   app_summary() << "  -m  meshfactor                     default: 1.0"           << '\n';
   app_summary() << "  -n  number of MC steps             default: 5"             << '\n';
   app_summary() << "  -N  number of MC substeps          default: 1"             << '\n';
+  app_summary() << "  -p  number of walkers per batch    default: 1"             << '\n';
   app_summary() << "  -P  not running pseudo potential   default: off"           << '\n';
   app_summary() << "  -r  set the acceptance ratio.      default: 0.5"           << '\n';
   app_summary() << "  -s  set the random seed.           default: 11"            << '\n';
@@ -225,7 +225,7 @@ int main(int argc, char** argv)
   int opt;
   while (optind < argc)
   {
-    if ((opt = getopt(argc, argv, "bhjPvVa:B:c:g:m:n:N:r:s:t:k:w:x:")) != -1)
+    if ((opt = getopt(argc, argv, "bhjPvVa:c:g:m:n:N:r:s:t:k:p:w:x:")) != -1)
     {
       switch (opt)
       {
@@ -234,9 +234,6 @@ int main(int argc, char** argv)
         break;
       case 'b':
         useRef = true;
-        break;
-      case 'B': // number of walkers per batch
-        nw_b = atoi(optarg);
         break;
       case 'c': // number of members per team
         team_size = atoi(optarg);
@@ -264,6 +261,9 @@ int main(int argc, char** argv)
         break;
       case 'N':
         nsubsteps = atoi(optarg);
+        break;
+      case 'p': // number of walkers per batch
+        nw_b = atoi(optarg);
         break;
       case 'P':
         run_pseudo = false;
