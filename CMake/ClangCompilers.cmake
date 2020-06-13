bb14a9da5399f79bc4971e9796d6545f94e14b29
Ye Luo <yeluo@anl.gov>
2020-06-12 20:30:22 -0500

    Change to explicit CrayPE handling.
    via -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment cmake option

--- CMake/ClangCompilers.cmake
+++ CMake/ClangCompilers.cmake
@@ -29,7 +29,7 @@ SET( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -ffast-ma
 #     arm: -mpcu
 #     default or cray: none
 #--------------------------------------
-IF($ENV{CRAYPE_VERSION} MATCHES ".")
+IF(CMAKE_SYSTEM_NAME STREQUAL "CrayLinuxEnvironment")
   # It's a cray machine. Don't do anything
 ELSEIF(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
   # the case for x86_64
