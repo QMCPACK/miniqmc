b986c7fc09de55da5d5548e90c198067572327f7
Ye Luo <yeluo@anl.gov>
2020-08-11 14:43:55 -0500

    Add target task unit test.

---
+++ src/OpenMP/tests/test_omp_target_task.cpp
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2019 QMCPACK developers.
+//
+// File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//
+// File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "catch.hpp"
+#include "Utilities/Configuration.h"
+
+namespace qmcplusplus
+{
+
+TEST_CASE("reduction with parallel for", "[openmp]")
+{
+ int a = 0;
+ #pragma omp target map(tofrom: a) depend(out: a) nowait
+ {
+   int sum = 0;
+   for (int i = 0; i < 100000; i++)
+     sum++;
+   a = 1;
+ }
+
+ #pragma omp task depend(in: a) shared(a)
+ {
+   REQUIRE(a == 1);
+ }
+}
+
+} // namespace qmcplusplus
