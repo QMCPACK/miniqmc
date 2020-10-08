46073436a432fc0472bf793784f9f87b5f8fdfcb
Ye Luo <yeluo@anl.gov>
2020-10-08 15:30:16 -0500

    Add a missing taskwait.

--- src/OpenMP/tests/test_omp_target_task.cpp
+++ src/OpenMP/tests/test_omp_target_task.cpp
@@ -18,19 +18,21 @@ namespace qmcplusplus
 
 TEST_CASE("reduction with parallel for", "[openmp]")
 {
- int a = 0;
- #pragma omp target map(tofrom: a) depend(out: a) nowait
- {
-   int sum = 0;
-   for (int i = 0; i < 100000; i++)
-     sum++;
-   a = 1;
- }
+  int a = 0;
+  #pragma omp target map(tofrom: a) depend(out: a) nowait
+  {
+    int sum = 0;
+    for (int i = 0; i < 100000; i++)
+      sum++;
+    a = 1;
+  }
 
- #pragma omp task depend(in: a) shared(a)
- {
-   REQUIRE(a == 1);
- }
+  #pragma omp task depend(in: a) shared(a)
+  {
+    REQUIRE(a == 1);
+  }
+
+  #pragma omp taskwait
 }
 
 } // namespace qmcplusplus
