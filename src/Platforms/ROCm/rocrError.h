4e6439772eb2880a4d1ea4a35193a7d268a73a01
Ye Luo <yeluo@anl.gov>
2020-10-09 01:05:28 -0500

    Correct ROCr error printing.

--- src/Platforms/ROCm/rocrError.h
+++ src/Platforms/ROCm/rocrError.h
@@ -28,13 +28,13 @@ inline void rocrAssert(hsa_status_t code, const std::string& cause, const char* 
 {
   if (code != HSA_STATUS_SUCCESS)
   {
-/*
+    const char *errStr = nullptr;
+    hsa_status_t errStr_status = hsa_status_string(code, &errStr);
     std::ostringstream err;
-    err << "rocrAssert: " << cudaGetErrorName(code) << " " << cudaGetErrorString(code) << ", file " << filename
+    err << "rocrAssert: " << (errStr_status == HSA_STATUS_SUCCESS ? errStr : "failed to get error string") << ", file " << filename
         << ", line " << line << std::endl
         << cause << std::endl;
     std::cerr << err.str();
-*/
     if (abort)
       throw std::runtime_error(cause);
   }
