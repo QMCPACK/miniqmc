a76dbc869810dc5a8d0661281b4155cb7bcf5e49
Ye Luo <yeluo@anl.gov>
2020-10-16 21:16:23 -0500

    Partial host device update test.

---
+++ src/Platforms/tests/OMPTarget/test_omp_partial_update.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2019 QMCPACK developers.
+//
+// File developed by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//
+// File created by: Ye Luo, yeluo@anl.gov, Argonne National Laboratory
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "catch.hpp"
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include "OMPTarget/OMPallocator.hpp"
+
+namespace qmcplusplus
+{
+
+const int array_size = 100;
+
+TEST_CASE("partial_update", "[openmp]")
+{
+  std::vector<int, OMPallocator<int>> array(array_size, 1);
+  int* array_ptr = array.data();
+
+  #pragma omp target teams distribute parallel for
+  for (int i = 0; i < array_size; i++)
+  {
+    array_ptr[i] += i;
+  }
+
+  REQUIRE(array_ptr[4] == 1);
+  #pragma omp target update from(array_ptr[4:array_size])
+  REQUIRE(array_ptr[4] == 5);
+}
+
+} // namespace qmcplusplus
