65eb8166b3c3b64a9594e0783547b982c9bbcffc
Ye Luo <yeluo@anl.gov>
2020-10-16 21:03:46 -0500

    Update tulip script.

--- config/build_tulip.sh
+++ config/build_tulip.sh
@@ -11,7 +11,7 @@ module load craype-accel-nvidia70
 folder=build_V100_Cray_offload_real_MP
 mkdir $folder
 cd $folder
-cmake -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DENABLE_OFFLOAD=1 -DQMC_MIXED_PRECISION=1 -DENABLE_TIMERS=1 -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment -DQMC_ENABLE_CUDA=ON ..
+cmake -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DENABLE_OFFLOAD=1 -DQMC_MIXED_PRECISION=1 -DENABLE_TIMERS=1 -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment -DQMC_ENABLE_CUDA=ON -DBLA_VENDOR=OpenBLAS ..
 make -j16
 cd ..
 module unload craype-accel-nvidia70
@@ -25,7 +25,7 @@ module load craype-accel-amd-gfx906
 folder=build_MI60_Cray_offload_real_MP
 mkdir $folder
 cd $folder
-cmake -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DENABLE_OFFLOAD=1 -DQMC_MIXED_PRECISION=1 -DENABLE_TIMERS=1 -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment -DQMC_ENABLE_ROCM=ON -DROCM_ROOT=$ROCM_PATH ..
+cmake -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DENABLE_OFFLOAD=1 -DQMC_MIXED_PRECISION=1 -DENABLE_TIMERS=1 -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment -DQMC_ENABLE_ROCM=ON -DROCM_ROOT=$ROCM_PATH -DBLA_VENDOR=OpenBLAS ..
 make -j16
 cd ..
 module unload craype-accel-amd-gfx906
