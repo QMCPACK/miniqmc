077681e3bf194ac6e4ac73ac38869c14c90cbf39
Ye Luo <yeluo@anl.gov>
2021-07-05 23:13:31 -0500

    Update CMake/unit_test.cmake

--- CMake/unit_test.cmake
+++ CMake/unit_test.cmake
@@ -1,17 +1,33 @@
 # Runs unit tests
-FUNCTION( ADD_UNIT_TEST TESTNAME TEST_BINARY )
-    MESSAGE("Adding test ${TESTNAME}")
-    IF ( USE_MPI )
-        ADD_TEST(NAME ${TESTNAME} COMMAND ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} 1 ${TEST_BINARY})
-        #SET_TESTS_PROPERTIES( ${TESTNAME} PROPERTIES ENVIRONMENT OMP_NUM_THREADS=1 )
-    ELSE()
-        ADD_TEST(NAME ${TESTNAME} COMMAND ${TEST_BINARY})
-        #SET_TESTS_PROPERTIES( ${TESTNAME} PROPERTIES ENVIRONMENT OMP_NUM_THREADS=1 )
-    ENDIF()
-
-    if (ENABLE_OFFLOAD OR QMC_ENABLE_CUDA OR QMC_ENABLE_ROCM OR QMC_ENABLE_ONEAPI)
-        set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK exclusively_owned_gpus)
+function(ADD_UNIT_TEST TESTNAME PROCS THREADS TEST_BINARY)
+  message(VERBOSE "Adding test ${TESTNAME}")
+  math(EXPR TOT_PROCS "${PROCS} * ${THREADS}")
+  if(HAVE_MPI)
+    add_test(NAME ${TESTNAME} COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${PROCS} ${MPIEXEC_PREFLAGS}
+                                      ${TEST_BINARY} ${ARGN})
+    set(TEST_ADDED TRUE)
+  else()
+    if((${PROCS} STREQUAL "1"))
+      add_test(NAME ${TESTNAME} COMMAND ${TEST_BINARY} ${ARGN})
+      set(TEST_ADDED TRUE)
+    else()
+      message(VERBOSE "Disabling test ${TESTNAME} (building without MPI)")
     endif()
+  endif()
 
-    SET_PROPERTY(TEST ${TESTNAME} APPEND PROPERTY LABELS "unit")
-ENDFUNCTION()
+  if(TEST_ADDED)
+    set_tests_properties(${TESTNAME} PROPERTIES PROCESSORS ${TOT_PROCS} ENVIRONMENT OMP_NUM_THREADS=${THREADS}
+                                                PROCESSOR_AFFINITY TRUE)
+
+    if(QMC_CUDA
+       OR ENABLE_CUDA
+       OR ENABLE_OFFLOAD)
+      set_tests_properties(${TESTNAME} PROPERTIES RESOURCE_LOCK exclusively_owned_gpus)
+    endif()
+  endif()
+
+  set_property(
+    TEST ${TESTNAME}
+    APPEND
+    PROPERTY LABELS "unit")
+endfunction()
