a9280f08519d08187e0108c40f8de830197bf187
Ye Luo <yeluo@anl.gov>
2021-10-29 17:30:38 -0500

    Expand test_omp_memory_interop.

--- src/Platforms/tests/OMPTarget/test_omp_memory_interop.cpp
+++ src/Platforms/tests/OMPTarget/test_omp_memory_interop.cpp
@@ -16,6 +16,7 @@
 #include <omp.h>
 #include "config.h"
 #include "PinnedAllocator.h"
+#include "OMPTarget/OMPallocator.hpp"
 
 namespace qmcplusplus
 {
@@ -23,12 +24,12 @@ namespace qmcplusplus
 const int array_size = 100;
 
 template<class T, class ALLOC>
-void test_device_memory_omp_access()
+void test_pinned_memory_omp_access()
 {
-  std::vector<T, ALLOC> array(array_size, 1);
+  std::vector<T, OMPallocator<T, ALLOC>> array(array_size, 1);
   int* array_ptr = array.data();
 
-  #pragma omp target teams distribute parallel for map(tofrom: array_ptr[:array_size])
+  #pragma omp target teams distribute parallel for map(always, tofrom: array_ptr[:array_size])
   for (int i = 0; i < array_size; i++)
   {
     array_ptr[i] += i;
@@ -37,22 +38,54 @@ void test_device_memory_omp_access()
   int sum = 0;
   for (int i = 0; i < array_size; i++)
     sum += std::abs(i + 1 - array[i]);
-  REQUIRE(sum == 0);
+  CHECK(sum == 0);
+}
+
+template<class T, class ALLOC>
+void test_vendor_device_memory_omp_access()
+{
+  ALLOC allocator;
+  int* array_ptr = allocator.allocate(array_size);
+
+  int sum = 0;
+  #pragma omp target teams distribute parallel for is_device_ptr(array_ptr) reduction(+:sum)
+  for (int i = 0; i < array_size; i++)
+  {
+    array_ptr[i] = i;
+    sum += array_ptr[i];
+  }
+
+#if defined(QMC_ENABLE_CUDA)
+  cudaErrorCheck(cudaMemset(array_ptr, 0, array_size * sizeof(int)), "cudaMemset failed on ALLOC memory!");
+#elif defined(QMC_ENABLE_ROCM)
+  hipErrorCheck(hipMemset(array_ptr, 0, array_size * sizeof(int)), "hipMemset failed on ALLOC memory!");
+#endif
+  CHECK(sum == (array_size - 1) * array_size / 2);
+  allocator.deallocate(array_ptr, array_size);
 }
 
 TEST_CASE("memory_interop", "[openmp]")
 {
 #if defined(QMC_ENABLE_CUDA) || defined(QMC_ENABLE_ROCM) || defined(QMC_ENABLE_ONEAPI)
-  test_device_memory_omp_access<int, PinnedAlignedAllocator<int>>();
-#endif
-#if defined(QMC_ENABLE_ROCM)
-  test_device_memory_omp_access<int, HIPHostAllocator<int>>();
+  test_pinned_memory_omp_access<int, PinnedAlignedAllocator<int>>();
 #endif
 
-  int* array = (int*)omp_target_alloc(array_size, omp_get_default_device());
+#if defined(QMC_ENABLE_ROCM)
+  test_pinned_memory_omp_access<int, HIPHostAllocator<int>>();
+#endif
+
 #if defined(QMC_ENABLE_CUDA)
+  test_vendor_device_memory_omp_access<int, CUDAAllocator<int>>();
+#elif defined(QMC_ENABLE_ROCM)
+  test_vendor_device_memory_omp_access<int, HIPAllocator<int>>();
+#endif
+
+  int* array = (int*)omp_target_alloc(array_size * sizeof(int), omp_get_default_device());
+#if defined(QMC_ENABLE_CUDA)
+  CUDAfill_n(array, array_size, 0);
   REQUIRE(isCUDAPtrDevice(array));
 #elif defined(QMC_ENABLE_ROCM)
+  hipErrorCheck(hipMemset(array, 0, array_size * sizeof(int)), "hipMemset failed on omp_target_alloc memory!");
   REQUIRE(isHIPPtrDevice(array));
 #endif
   omp_target_free(array, omp_get_default_device());
