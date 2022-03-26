8b407ec0959df516e5ca9fded1481186f5f59f77
Ye Luo <yeluo@anl.gov>
2022-03-26 12:22:38 -0500

    Add DT offload in DistanceTableBA evaluate.

--- src/Particle/DistanceTableBA.h
+++ src/Particle/DistanceTableBA.h
@@ -33,6 +33,7 @@ struct DistanceTableBA : public DTD_BConds<T, D, SC>, public DistanceTableData
       : DTD_BConds<T, D, SC>(source.Lattice), DistanceTableData(source, target)
   {
     resize(source.getTotalNum(), target.getTotalNum());
+    PRAGMA_OFFLOAD("omp target enter data map(to : this[:1])")
   }
 
   void resize(int ns, int nt)
@@ -57,14 +58,50 @@ struct DistanceTableBA : public DTD_BConds<T, D, SC>, public DistanceTableData
 
   DistanceTableBA()                       = delete;
   DistanceTableBA(const DistanceTableBA&) = delete;
-  ~DistanceTableBA() {}
+
+  ~DistanceTableBA() { PRAGMA_OFFLOAD("omp target exit data map(delete : this[:1])") }
 
   /** evaluate the full table */
   inline void evaluate(ParticleSet& P)
   {
+    /*
     // be aware of the sign of Displacement
     for (int iat = 0; iat < Ntargets; ++iat)
       DTD_BConds<T, D, SC>::computeDistances(P.R[iat], Origin->RSoA, Distances[iat], Displacements[iat], 0, Nsources);
+    */
+
+    const size_t ntgt_local  = Ntargets;
+    const size_t ntgt_padded = getAlignedSize<T>(Ntargets);
+    const size_t nsrc_padded = getAlignedSize<T>(Nsources);
+
+    auto* dist_ptr  = Distances.data();
+    auto* displ_ptr = memoryPool.data();
+    auto* src_ptr   = Origin->RSoA.data();
+    auto* tgt_ptr   = P.RSoA.data();
+
+    const int ChunkSizePerTeam = 128;
+    const size_t num_teams     = (nsrc_padded + ChunkSizePerTeam - 1) / ChunkSizePerTeam;
+
+    PRAGMA_OFFLOAD("omp target teams distribute collapse(2) num_teams(Ntargets * num_teams) \
+                    map(to: src_ptr[:D*Origin->RSoA.capacity()], tgt_ptr[:D*P.RSoA.capacity()]) \
+                    map(from: dist_ptr[:Distances.size()], displ_ptr[:memoryPool.size()])")
+    for (size_t iat = 0; iat < ntgt_local; ++iat)
+      for (size_t team_id = 0; team_id < num_teams; ++team_id)
+      {
+        T pos[D];
+        for (int idim = 0; idim < D; idim++)
+          pos[idim] = *(tgt_ptr + ntgt_padded * idim + iat);
+
+        const size_t first = ChunkSizePerTeam * team_id;
+        const size_t last  = std::min(first + ChunkSizePerTeam, nsrc_padded);
+
+        PRAGMA_OFFLOAD("omp parallel for")
+        for (size_t jel = first; jel < last; ++jel)
+        {
+          DTD_BConds<T, D, SC>::computeDistancesOffload(pos, src_ptr, nsrc_padded, dist_ptr + nsrc_padded * iat,
+                                                        displ_ptr + nsrc_padded * D * iat, nsrc_padded, jel);
+        }
+      }
   }
 
   /** evaluate the iat-row with the current position
